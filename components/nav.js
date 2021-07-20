f6f5a5500b7fddaa4d5afd0c051903149c66da0a
Khushi <dpskhu13108@gmail.com>
2021-07-20 23:59:06 +0530

    added

--- components/nav.js
+++ components/nav.js
@@ -1,43 +1,40 @@
 
 
-const Nav = () => 
-  <nav>
-    <div className='row'>
-        <ul>
-                 <li><a className="nav-link" href="#">Home</a></li>
-                 <li><a className="nav-link" href="#">Drones</a></li>
-                 <li><a className="nav-link" href="#">Check In</a></li>
-                 <li><a className="nav-link" href="#">Blogs</a></li>
-                 <li><a className="nav-link" href="#">Contact Us</a></li>
-        </ul>
-      </div>
+const Nav = ()=> 
 
-    <style jsx>{`
-      :global(body) {
-        margin: 0;
-        font-family: -apple-system,BlinkMacSystemFont,Avenir Next,Avenir,Helvetica,sans-serif;
-      }
-      ul{
-        list-style: none
-      }
-      div {
-        margin:100px 0px 80px 80px;
-
-      }
-      nav{
-        display: flex;
-      }
-      li{
-        padding: 0px 100px 0px 83px;
-        display: inline;
-        style: none;
-      }
-      a{
-        text-decoration: inherit;
-        color:inherit;
-      }
-    `}</style>
-  </nav>
+<nav className="relative flex flex-wrap items-center justify-between px-2 py-3 navbar-expand-lg bg-red-500 mb-3">
+  <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
+    <div className="w-full relative flex justify-between lg:w-auto  px-4 lg:static lg:block lg:justify-start">
+      <a className="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-no-wrap uppercase text-white" href="#pablo">
+        red Color
+      </a>
+      <button className="cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none" type="button">
+        <span className="block relative w-6 h-px rounded-sm bg-white"></span>
+        <span className="block relative w-6 h-px rounded-sm bg-white mt-1"></span>
+        <span className="block relative w-6 h-px rounded-sm bg-white mt-1"></span>
+      </button>
+    </div>
+    <div className="lg:flex flex-grow items-center">
+      <ul className="flex flex-col lg:flex-row list-none ml-auto">
+          <li className="nav-item">
+            <a className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75" href="#pablo">
+              Discover
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75" href="#pablo">
+              Profile
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75" href="#pablo">
+              Setting
+            </a>
+          </li>
+      </ul>
+    </div>
+  </div>
+</nav>
 ;
 
 export default Nav;
