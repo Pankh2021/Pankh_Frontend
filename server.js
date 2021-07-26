42852a0122f7d680e2849a8cb9a179069c53654f
khadyothan <dasarikhadyothan@gmail.com>
2021-07-26 20:23:30 +0530

    edit server.js

--- server.js
+++ server.js
@@ -1,16 +1,19 @@
 const express = require("express");
 const next = require("next");
+const cors = require("cors");
 const dev = process.env.NODE_ENV !== "production";
 
-const app = next({ dev });
-const handle = app.getRequestHandler();
+const server = next({ dev });
+const handle = server.getRequestHandler();
 
-app.prepare().then(() => {
-    const server = express();
-    server.get("*", (req, res) => {
+server.prepare().then(() => {
+    const app = express();
+    app.use(cors());
+
+    app.get("*", (req, res) => {
         return handle(req, res);
     });
-    server.listen(3030, (err) => {
+    app.listen(3030, (err) => {
         if(err) throw err
         console.log("server ready");
     });
