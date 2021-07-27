26e410a0b05e795a2db1297e13b6d2b22b6962ec
Khushi <dpskhu13108@gmail.com>
2021-07-27 15:59:57 +0530

    added

--- components/nav.js
+++ components/nav.js
@@ -55,7 +55,7 @@ function Nav() {
           <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-6 h-6"><path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
         </button>
       </span>
-      <input type="search" name="q" class="py-2 text-sm text-white bg-gray-900 rounded-md pl-10 focus:outline-none focus:bg-white focus:text-gray-900" placeholder="Search..." autocomplete="off"/>
+      <input type="search" name="q" class="py-4 ml-10 mr-20 w-full text-sm text-white bg-gray-200 rounded-md pl-10 focus:outline-none  focus:text-gray-900" placeholder="Search by brand,category, application, etc...." autoComplete='off' />
     </div>
   </form>
 </div>
