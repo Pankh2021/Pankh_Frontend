432435742c1b84fe00df8e3d481f5cad714391af
Khushi <dpskhu13108@gmail.com>
2021-07-30 23:40:23 +0530

    added

--- components/location.js
+++ components/location.js
@@ -6,16 +6,17 @@ function Location(){
     return (
         <>
 
-<div class="flex flex-col bg-white m-auto p-auto">
-<h1 class="flex py-5 lg:px-20 md:px-10 px-5 lg:mx-40 md:mx-20 mx-5 font-bold text-4xl text-gray-800">
+<div class="flex flex-col m-auto p-auto bg-gradient-to-r from-yellow-400 via-red-500 to-pink-500">
+
+      <div
+        class="flex overflow-x-scroll pt-20 pb-20 hide-scroll-bar"
+      >
+        <h1 class="flex py-5 lg:px-20 md:px-10 px-3 lg:mx-40 md:mx-20 mx-5 font-bold text-4xl text-gray-800">
         Search Drones by city
       </h1>
-      <div
-        class="flex overflow-x-scroll pb-10 hide-scroll-bar"
-      >
         <div
           class="flex flex-nowrap lg:ml-40 md:ml-20 ml-10 "
-        >
+          >
           <div class="inline-block px-3">
             <div
               class="w-64 h-64 max-w-xs overflow-hidden rounded-lg shadow-md bg-white hover:shadow-xl transition-shadow duration-300 ease-in-out"
@@ -34,7 +35,7 @@ function Location(){
           <div class="inline-block px-3">
             <div
               class="w-64 h-64 max-w-xs overflow-hidden rounded-lg shadow-md bg-white hover:shadow-xl transition-shadow duration-300 ease-in-out"
-            >
+              >
 
 <div class="flex flex-col items-center justify-center bg-white p-4 shadow rounded-lg">
 				<div class="inline-flex shadow-lg border border-gray-200 rounded-full overflow-hidden h-40 w-40">
@@ -49,10 +50,10 @@ function Location(){
           <div class="inline-block px-3">
             <div
               class="w-64 h-64 max-w-xs overflow-hidden rounded-lg shadow-md bg-white hover:shadow-xl transition-shadow duration-300 ease-in-out"
-            >
+              >
               <div class="flex flex-col items-center justify-center bg-white p-4 shadow rounded-lg">
 				<div class="inline-flex shadow-lg border border-gray-200 rounded-full overflow-hidden h-40 w-40">
-					<img src="/static/images/hawa-mahal.png"
+          <img src="/static/images/hawa-mahal.png"
 						 alt=""
 						 class="h-full w-full"/>
 				</div>
@@ -77,7 +78,7 @@ function Location(){
           <div class="inline-block px-3">
             <div
               class="w-64 h-64 max-w-xs overflow-hidden rounded-lg shadow-md bg-white hover:shadow-xl transition-shadow duration-300 ease-in-out"
-            >
+              >
               <div class="flex flex-col items-center justify-center bg-white p-4 shadow rounded-lg">
 				<div class="inline-flex shadow-lg border border-gray-200 rounded-full overflow-hidden h-40 w-40">
 					<img src="/static/images/surat-municipal-corporation.png"
@@ -105,7 +106,7 @@ function Location(){
           <div class="inline-block px-3">
             <div
               class="w-64 h-64 max-w-xs overflow-hidden rounded-lg shadow-md bg-white hover:shadow-xl transition-shadow duration-300 ease-in-out"
-            >
+              >
               <div class="flex flex-col items-center justify-center bg-white p-4 shadow rounded-lg">
 				<div class="inline-flex shadow-lg border border-gray-200 rounded-full overflow-hidden h-40 w-40">
 					<img src="/static/images/chennai.png"
