d9666ba2d058f19352e0caaf0a53629742e3dfae
Khushi <dpskhu13108@gmail.com>
2021-07-30 23:05:09 +0530

    added

--- components/clients.js
+++ components/clients.js
@@ -9,9 +9,11 @@ function Client(){
 <section class="mx-auto bg-white">
             <div class="container px-5 mx-auto lg:px-24 lg:py-20">
               <div class="flex flex-col w-full mb-12 text-center">
-                <h2 class="mb-4 text-xs font- tracking-widest text-black uppercase title-font"> Our Clients
+              <h2 class="mb-4 text-2xl font-2xl tracking-widest text-black uppercase title-font"> Our Clients
                 </h2>
-                <div class="divider-5">OR</div> 
+                <div class="container" >
+                <hr class="hr-text" data-content="AND"/>
+                </div>
               </div>
               <div class="grid grid-cols-3 gap-16 text-center lg:grid-cols-6">
                 <div class="flex items-center justify-center ">
