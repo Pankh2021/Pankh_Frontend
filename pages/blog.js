d761fc46ec455f2764d0b1c349d665448dd1de8b
Khushi <dpskhu13108@gmail.com>
2021-08-25 01:02:32 +0530

    added

--- pages/blog.js
+++ pages/blog.js
@@ -1,21 +1,21 @@
 import React from 'react';
+import Footerr from '../components/footer';
 import Nav from '../components/nav';
 
 
 function Blog() {
     return (
         <>
-        
-<section class="text-gray-600 body-font">
+      <Nav></Nav>  
+<section class="text-gray-600 body-font bg-white">
   <div class="container px-5 py-24 mx-auto">
     <div class="flex flex-wrap -m-4">
       <div class="p-4 md:w-1/3">
         <div class="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
-          <img class="lg:h-48 md:h-36 w-full object-cover object-center" src="https://dummyimage.com/720x400" alt="blog"/>
+          <img class="lg:h-48 md:h-36 w-full object-cover object-center" src="/static/assets(png)/image-3.png" alt="blog"/>
           <div class="p-6">
-            <h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">CATEGORY</h2>
-            <h1 class="title-font text-lg font-medium text-gray-900 mb-3">The Catalyzer</h1>
-            <p class="leading-relaxed mb-3">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.</p>
+            <h1 class="title-font text-lg font-medium text-gray-900 mb-3">Drones in the construction</h1>
+            <p class="leading-relaxed mb-3">Drones have been underutilized in the construction industry for a long time now. But with the changing technology trends, the construction industry is adapting itself too. Drones have increased safety and accuracy in this sector alongside reducing the total cost of the project. Lack of knowledge about drones and their capabilities in the field of construction is a liability.</p>
             <div class="flex items-center flex-wrap ">
               <a class="text-purple-500 inline-flex items-center md:mb-2 lg:mb-0">Learn More
                 <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
@@ -23,28 +23,20 @@ function Blog() {
                   <path d="M12 5l7 7-7 7"></path>
                 </svg>
               </a>
-              <span class="text-gray-400 mr-3 inline-flex items-center lg:ml-auto md:ml-0 ml-auto leading-none text-sm pr-3 py-1 border-r-2 border-gray-200">
-                <svg class="w-4 h-4 mr-1" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24">
-                  <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
-                  <circle cx="12" cy="12" r="3"></circle>
-                </svg>1.2K
-              </span>
-              <span class="text-gray-400 inline-flex items-center leading-none text-sm">
-                <svg class="w-4 h-4 mr-1" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24">
-                  <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
-                </svg>6
-              </span>
+            
             </div>
           </div>
         </div>
       </div>
       <div class="p-4 md:w-1/3">
         <div class="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
-          <img class="lg:h-48 md:h-36 w-full object-cover object-center" src="https://dummyimage.com/721x401" alt="blog"/>
+          <img class="lg:h-48 md:h-36 w-full object-cover object-center" src="/static/assets(png)/image-2.png" alt="blog"/>
           <div class="p-6">
-            <h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">CATEGORY</h2>
-            <h1 class="title-font text-lg font-medium text-gray-900 mb-3">The 400 Blows</h1>
-            <p class="leading-relaxed mb-3">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.</p>
+            <h1 class="title-font text-lg font-medium text-gray-900 mb-3">One stop drop platform</h1>
+            <p class="leading-relaxed mb-3">Beyond the predictable deliverables, aerial site intelligence is more beneficial for construction
+projects. Drones used to capture construction progress are a sure-fire way of collecting all
+relevant data, as is and without prejudice, in a much faster and effective manner. More than half of construction projects fall short of their timelines and budgets. Using drone technology in construction will assist in meeting these goals without incurring exorbitant costs. Drones will help you get the job done, whether for evaluation, data collection, or transportation. Drones have already proven their worth to a few construction companies. They're constantly looking for ways to put them to use for their company and customers. If the use of drones in construction grows, further advances can benefit the construction industry and beyond.
+</p>
             <div class="flex items-center flex-wrap">
               <a class="text-purple-500 inline-flex items-center md:mb-2 lg:mb-0">Learn More
                 <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
@@ -52,17 +44,7 @@ function Blog() {
                   <path d="M12 5l7 7-7 7"></path>
                 </svg>
               </a>
-              <span class="text-gray-400 mr-3 inline-flex items-center lg:ml-auto md:ml-0 ml-auto leading-none text-sm pr-3 py-1 border-r-2 border-gray-200">
-                <svg class="w-4 h-4 mr-1" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24">
-                  <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
-                  <circle cx="12" cy="12" r="3"></circle>
-                </svg>1.2K
-              </span>
-              <span class="text-gray-400 inline-flex items-center leading-none text-sm">
-                <svg class="w-4 h-4 mr-1" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24">
-                  <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
-                </svg>6
-              </span>
+              
             </div>
           </div>
         </div>
@@ -99,6 +81,8 @@ function Blog() {
     </div>
   </div>
 </section>
+
+<Footerr></Footerr>
     </>
     )
 }
