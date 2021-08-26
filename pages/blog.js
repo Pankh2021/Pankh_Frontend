4d96978124775513915335575c1d607f92ea1183
Khushi <dpskhu13108@gmail.com>
2021-08-26 15:01:59 +0530

    added blog

--- pages/blog.js
+++ pages/blog.js
@@ -73,8 +73,8 @@ relevant data, as is and without prejudice, in a much faster and effective manne
           <img class="lg:h-48 md:h-36 w-full object-cover object-center" src="https://dummyimage.com/722x402" alt="blog"/>
           <div class="p-6">
             
-            <h1 class="title-font text-lg font-medium text-gray-900 mb-3"> Ortho mosaic map</h1>
-            <p class="leading-relaxed mb-3">Ortho mosaic maps - represent the area of interest with a high-resolution image captured from the top at 90 degrees. In the field of construction, this can be utilized to receive the constant progress of the construction site and measure true distances. Raw data from the survey can be integrated with software to get the 3D model of the construction site. </p>
+            <h1 class="title-font text-lg font-medium text-gray-900 mb-3"> Surface Model</h1>
+            <p class="leading-relaxed mb-3">Digital Surface Model- The measurement takes top portions of buildings, trees, and other objects in the area of interest. Similar to DEM even DSM utilizes colour to indicate the elevations of the subjects.<br/> Digital Terrain Model- A 3D model of the terrain plotted using the data obtained using DSM. This model gives an overall geographical representation of the area of interest </p>
             <div class="flex items-center flex-wrap ">
               <a class="text-purple-500 inline-flex items-center md:mb-2 lg:mb-0">Learn More
                 <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
@@ -91,29 +91,41 @@ relevant data, as is and without prejudice, in a much faster and effective manne
           <img class="lg:h-48 md:h-36 w-full object-cover object-center" src="https://dummyimage.com/722x402" alt="blog"/>
           <div class="p-6">
             
-            <h1 class="title-font text-lg font-medium text-gray-900 mb-3"> Ortho mosaic map</h1>
-            <p class="leading-relaxed mb-3">It is important to understand the purpose of drones in each stage and appreciate the amount of progress that can be made with them during a project.
+            <h1 class="title-font text-lg font-medium text-gray-900 mb-3"> Counter Lines</h1>
+            <p class="leading-relaxed mb-3">Contour lines- Lines connecting points of equal elevation. This gives an insight into the type of terrain and the height and depth of the terrain as well. 
+ </p>
+            <div class="flex items-center flex-wrap ">
+              <a class="text-purple-500 inline-flex items-center md:mb-2 lg:mb-0">Learn More
+                <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                  <path d="M5 12h14"></path>
+                  <path d="M12 5l7 7-7 7"></path>
+                </svg>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="p-4 md:w-1/3">
+        <div class="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
+          <img class="lg:h-48 md:h-36 w-full object-cover object-center" src="https://dummyimage.com/722x402" alt="blog"/>
+          <div class="p-6">
+            
+            <h1 class="title-font text-lg font-medium text-gray-900 mb-3"> Four Phases</h1>
+            <p class="leading-relaxed mb-3">It is important to understand the purpose of drones in each stage and appreciate the amount of progress that can be made with them during a project. <br/>
 
-Initiation: Every venture begins with a piece of land. An aerial analysis of the land can quickly
-capture the territory and its challenges, allowing informed decisions about how development can
-occur. Upon request, specific geological guides and 3D models can be obtained to produce a lot more information on the direction of work.
-
+Initiation: Every venture begins with a piece of land. 
+<br/>
 Planning: Project plans are made based on the information gathered during the overview.
-During this time, drone imaging will showcase the current reality of the land's difficulties for
-the development plan, rather than leaving it to suspicion. GIS planning also allows for 3D imaging of what the project will look like when it is completed.
 
-Execution: This is the most crucial stage of growth as everything starts happening at the same
-time. Drone tours are a viable alternative to on-site tours. It gives a birds-eye view to the
+<br/>
+Execution: Drone tours are a viable alternative to on-site tours. It gives a birds-eye view to the
 customers and other stakeholders. Also, it allows to take pictures of construction sites and
 provides stakeholders and consumers with real-time alerts. The ability to monitor the progress remotely and store data in the cloud makes it easier to carry worksites to the workplace.
-It increases customer satisfaction and acts as a customer magnet to sell to more customers.
-
+<br/>
 Handover: At this stage of construction, from the outside, everything appears fine until the
 project is completed. It takes drones' technological prowess to detect any heat leaks with their
-thermal imaging feature. Drones keep track of the "invisible"—what flows through the walls,
-underground pipes, and so on. It uses technology to match the model with the output of
-construction and checks to identify any deviation and anomalies from the design drawings.
-
+thermal imaging feature.
+<br/>
 The mentioned features above shall help increase the speed of work and reduce the cost needed to perform the same task using manpower and reduce the risk of accidents. 
 
  </p>
